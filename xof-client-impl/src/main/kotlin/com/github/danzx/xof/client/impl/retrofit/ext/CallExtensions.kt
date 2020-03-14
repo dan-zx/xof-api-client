b1f1b09fcddf5f0d95de4fa094fcf4aea40f511e
dan-zx <danyboy_shaggy@hotmail.com>
2020-03-13 22:29:26 -0600

    Add UsersApiRetrofitAdapter

---
+++ xof-client-impl/src/main/kotlin/com/github/danzx/xof/client/impl/retrofit/ext/CallExtensions.kt
@@ -0,0 +1,25 @@
+package com.github.danzx.xof.client.impl.retrofit.ext
+
+import com.github.danzx.xof.client.XofApiException
+import com.github.danzx.xof.client.XofClientException
+
+import retrofit2.Call
+
+private fun <T> Call<T>.doGet(): T? {
+    val response = execute()
+    if (!response.isSuccessful) throw XofApiException(response.code(), response.errorBody()?.string())
+    return response.body()
+}
+
+fun <T> Call<T>.get() = doGet() ?: throw XofClientException("Expected body in response but got null!")
+
+fun <T> Call<T>.getOrNullOnNotFound(): T? {
+    try {
+        return doGet()
+    } catch (ex: XofApiException) {
+        if (ex.statusCode == 404) return null
+        throw ex
+    }
+}
+
+fun Call<Unit>.call() { doGet() }
